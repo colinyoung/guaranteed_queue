7549c160f9c6c88a7e25f1dd778d09bfa8df4a88
Colin Young <me@colinyoung.com>
2014-03-10 14:55:46 -0500

    Initialize module first

--- lib/guaranteed_queue/logger.rb
+++ lib/guaranteed_queue/logger.rb
@@ -2,69 +2,71 @@ require 'logger'
 
 require 'colorize'
 
-class GuaranteedQueue::Logger < ::Logger
+module GuaranteedQueue
+  class Logger < ::Logger
 
-  def initialize *args
-    super *args unless args.empty?
+    def initialize *args
+      super *args unless args.empty?
 
-    filedev = self.class.log_path
-    super(filedev)
-  end
+      filedev = self.class.log_path
+      super(filedev)
+    end
 
-  def bright text, message=nil
-    info "* #{text}".magenta
-  end
+    def bright text, message=nil
+      info "* #{text}".magenta
+    end
 
-  def warn text, message=nil
-    super "! #{text}#{' ' + stringify(message) if message}".yellow
-  end
+    def warn text, message=nil
+      super "! #{text}#{' ' + stringify(message) if message}".yellow
+    end
 
-  def error text, message=nil
-    super "! #{text}#{' ' + stringify(message) if message}".red
-  end
+    def error text, message=nil
+      super "! #{text}#{' ' + stringify(message) if message}".red
+    end
 
-  def success text, message=nil
-    info "! #{text}#{' ' + stringify(message) if message}".green
-  end
+    def success text, message=nil
+      info "! #{text}#{' ' + stringify(message) if message}".green
+    end
 
-  def start text, message=nil
-    info "^ #{text}#{' ' + stringify(message) if message}".colorize(:green).on_white
-  end
+    def start text, message=nil
+      info "^ #{text}#{' ' + stringify(message) if message}".colorize(:green).on_white
+    end
 
-  def stop text, message=nil
-    info "$ #{text}#{' ' + stringify(message) if message}".colorize(:red).on_white
-  end
+    def stop text, message=nil
+      info "$ #{text}#{' ' + stringify(message) if message}".colorize(:red).on_white
+    end
 
-  def message_sent text
-    info "> #{text}".cyan
-  end
+    def message_sent text
+      info "> #{text}".cyan
+    end
 
-  def message_received msg
-    info "< #{stringify msg}".green
-  end
+    def message_received msg
+      info "< #{stringify msg}".green
+    end
 
-  class << self
+    class << self
 
-    [:info, :bright, :warn, :error, :success, :start, :stop, :message_sent, :message_received].each do |method|
-      define_method method do |*args|
-        $GQ_LOG.send(method, *args)
+      [:info, :bright, :warn, :error, :success, :start, :stop, :message_sent, :message_received].each do |method|
+        define_method method do |*args|
+          $GQ_LOG.send(method, *args)
+        end
       end
-    end
 
-    def log_path
-      "#{Dir.pwd}/log/guaranteed_queue.log"
-    end
+      def log_path
+        "#{Dir.pwd}/log/guaranteed_queue.log"
+      end
 
-    def prefix
-      "[GuaranteedQueue]"
-    end
+      def prefix
+        "[GuaranteedQueue]"
+      end
 
-    def stringify msg
-      "#{msg.id[0...6]} (#{msg.body})"
+      def stringify msg
+        "#{msg.id[0...6]} (#{msg.body})"
+      end
+
     end
 
   end
-
 end
 
 $GQ_LOG = GuaranteedQueue::Logger.new.tap do |logger|
