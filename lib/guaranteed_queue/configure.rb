64d79316b2b8b3a743cd3ef90ee31cfbd7532f04
Colin Young <me@colinyoung.com>
2014-03-10 22:32:23 -0500

    Allow for development rack to block queues

--- lib/guaranteed_queue/configure.rb
+++ lib/guaranteed_queue/configure.rb
@@ -6,5 +6,5 @@ GuaranteedQueue.config(
   region: ENV['AWS_REGION'] || 'us-east-1',
   dead_letter_poll_interval_seconds: 30,
   message_failures_allowed: 0,
-  stub_requests: ENV['RACK_ENV'] == 'test' || ENV['RAILS_ENV'] == 'test'
+  stub_requests: ENV['RACK_ENV'] == 'test' || ENV['RAILS_ENV'] == 'test' || ENV['RAILS_ENV'] == 'development'
 )
