ade37bcc792b590b03398ff941b72473889be399
Colin Young <me@colinyoung.com>
2014-10-22 11:13:30 -0500

    Use config queue name

--- lib/guaranteed_queue/manager.rb
+++ lib/guaranteed_queue/manager.rb
@@ -56,7 +56,7 @@ module GuaranteedQueue
       host = %x{hostname}.split('.').first rescue nil
       return queues unless defined? ::Rails
       queues.select do |q|
-        if queue_name = (@options[:queue_name] || ENV['GUARANTEED_QUEUE_NAME'])
+        if queue_name = (@options[:queue_name] || config[:queue_name] || ENV['GUARANTEED_QUEUE_NAME'])
           q.url[/#{queue_name}$/]
         elsif queue_env = config[:queue_env]
           q.url.downcase.include? queue_env
